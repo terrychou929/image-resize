15842c39ba7ab6a12de138860ddfb8ea6d419916
unknown <terrychou929@gmail.com>
2024-11-07 01:05:34 +0800

    feat(main): add exception handler

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 
 
 def resize_images(input_folder, output_folder, scale=0.1):
+    err_list = []
+    Image.LOAD_TRUNCATED_IMAGES = True
 
     # Check folders exist
     os.makedirs(output_folder, exist_ok=True)
@@ -11,15 +13,22 @@ def resize_images(input_folder, output_folder, scale=0.1):
 
     # Loop through files in input folder
     for filename in os.listdir(input_folder):
+        print(f"Start resizing {filename}")
         if filename.lower().endswith(supported_formats):
             input_path = os.path.join(input_folder, filename)
-            
             with Image.open(input_path) as img:
-                new_size = (int(img.width * scale), int(img.height * scale))
-                resized_img = img.resize(new_size, Image.LANCZOS)
-                output_path = os.path.join(output_folder, filename)
-                resized_img.save(output_path)
-                print(f"Image {filename} resized and saved to {output_path}")
+                try:
+                    new_size = (int(img.width * scale), int(img.height * scale))
+                    resized_img = img.resize(new_size, Image.LANCZOS)
+                    output_path = os.path.join(output_folder, filename)
+                    resized_img.save(output_path)
+                    print(f"Image {filename} resized and saved to {output_path}")
+                except:
+                    print(f"Error on {filename}, skipping it...")
+                    err_list.append(filename)
+
+    print("Error List:")
+    print(err_list)
 
 
 if __name__ == '__main__':
@@ -31,5 +40,5 @@ if __name__ == '__main__':
     scale = float(sys.argv[1])
     input_folder = "./input"
     output_folder = "./output"
-    
+
     resize_images(input_folder, output_folder, scale)
